ea22cf1c1ec8883d51f3f3faff325d5664958be3
Stephane <Stephane@192.168.1.22>
2015-12-07 18:38:43 +0100

    first init

---
+++ CTS/src/com/skula/cts/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.skula.cts.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.skula.cts.R;
+
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+	}
+
+}
